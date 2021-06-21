992224a6111ebc13df2c7033f1bd2902d1e0b5ac
ggh5454 <ggh5454@naver.com>
2021-06-21 16:11:11 +0900

    MainActivity 1

--- app/src/main/java/com/example/aoppart2chapter6/MainActivity.kt
+++ app/src/main/java/com/example/aoppart2chapter6/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.example.aoppart2chapter6
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+
+    private val remainMinutesTextView2: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView2)
+    }
+
+    private val seekBar1: SeekBar by lazy {
+        findViewById(R.id.seekBar1)
+    }
+
+    private val seekBar2: SeekBar by lazy {
+        findViewById(R.id.seekBar2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews(){
+        seekBar1.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener{
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    remainMinutesTextView.text="%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
+
+        seekBar2.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener{
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    remainMinutesTextView2.text="%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
     }
 }
